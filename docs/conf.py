e5b222e90cfe0c1d8a9ac317cdb7ecb553a9a95e
Adam8234 <Acorpstein8234@gmail.com>
2016-08-16 04:11:27 -0500

    Fixed theme. Added Markdown Support.

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,10 @@ extensions = []
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
@@ -119,7 +123,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
